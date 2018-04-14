c16d3af99e322bd4cf70d70786df0213749e1bd2
Teddy1LeBunny <racmtb@gmail.com>
2018-04-14 15:58:39 -0300

    listar tarefas

---
+++ test_todo.py
@@ -0,0 +1,31 @@
+from todo import app, tarefas
+
+
+def test_listar_tarefa_deve_retornar_status_200():
+    with app.test_client() as cliente:
+        resposta = cliente.get('/tarefas')
+        assert resposta.status_code == 200
+
+
+def testlistar_tarefas_deve_ter_formato_json():
+    with app.test_client() as cliente:
+        resposta = cliente.get('/tarefas')
+        assert resposta.content_type == 'application/json'
+
+
+def test_lista_de_tarefas_vazia_deve_retornar_lista_vazia():
+    with app.test_client() as cliente:
+        resposta = cliente.get('/tarefas')
+        assert resposta.data == b'[]\n'
+
+
+def test_lista_de_tarefas__nao_vazia_retornar_conteudo():
+    tarefas.append({'id': 1, 'titulo': 'tarefa 1',
+                    'descricao': 'tarefa de numero 1', 'estado': False}) 
+    with app.test_client() as cliente:
+        resposta = cliente.get('/tarefas')
+        assert resposta.data == (b'[\n  {\n    "descricao": '
+                                 b'"tarefa de numero 1", \n    '
+                                 b'"estado": false, \n    '
+                                 b'"id": 1, \n    '
+                                 b'"titulo": "tarefa 1"\n  }\n]\n')
